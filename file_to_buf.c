9dd428569e9b0401eace307357242440c06cbff7
brisauce <brisauce69@gmail.com>
2026-07-07 16:47:32 -0700

    v0.1.1
    Fixed buggy copyFileToBuf function.

--- file_to_buf.c
+++ file_to_buf.c
@@ -7,15 +7,24 @@
 
 void copyFileToBuf(buffer * buff, FILE * source_fp)
 {
-  assert(buff->buf);
-  size_t bytes_read;
-  while( (bytes_read = fread(buff->buf, 1, buff->buf_size, source_fp)) > 0 )
+  //  read data in 
+  //  move pointer to next empty space
+  //  check if reading more in will cause an overflow and if so, reallocate
+  //  set the read size to the remaining size of the buffer
+  //  repeat
+  size_t read_size = buff->buf_size;
+  size_t next_empty_space = 0;
+  size_t read;
+  while ((read = fread (buff->buf + next_empty_space, sizeof(char), read_size, source_fp)) != 0)
   {
-    if (bytes_read == buff->buf_size)
+    next_empty_space += read;
+    if (next_empty_space + read_size > buff->buf_size)
     {
       buff->buf_size *= 2;
-      buff->buf = realloc(buff->buf, buff->buf_size);
+      buff->buf = realloc(buff->buf, buff->buf_size * sizeof(char));
       assert(buff->buf);
     }
+    read_size = buff->buf_size - next_empty_space;
   }
 }
+
--- file_to_buf.h
+++ file_to_buf.h
@@ -1,7 +1,7 @@
 #ifndef FILE_TO_BUF_H
 #define FILE_TO_BUF_H
 #include <stdio.h>
-#include "build/create_c_fileConfig.h" 
+#include "build/create_c_fileConfig.h"
 
 void copyFileToBuf(buffer * buff, FILE * source_fp);
 
